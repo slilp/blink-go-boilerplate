ef272fcc5f684c26055c6e8dffbd6590364cacb9
slil_puangpoom <slil_puangpoom@gmail.com>
2023-08-22 19:47:30 +0700

    chore : pq mode config

--- database/config.go
+++ database/config.go
@@ -8,11 +8,12 @@ type Config struct {
 	PostgresHost     string `envconfig:"POSTGRES_HOST" default:"localhost"` 
 	PostgresPort     string `envconfig:"POSTGRES_PORT" default:"5432"`
 	PostgresDB       string `envconfig:"POSTGRES_DB" default:"yourdb"`
+	PostgresMode     string `envconfig:"POSTGRES_MODE" default:"disable"`
 }
 
 func (c *Config) generateDSN() string {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s database=%s port=%s sslmode=require",
-		c.PostgresHost, c.PostgresUser, c.PostgresPassword, c.PostgresDB, c.PostgresPort)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s database=%s port=%s sslmode=%s",
+		c.PostgresHost, c.PostgresUser, c.PostgresPassword, c.PostgresDB, c.PostgresPort,c.PostgresMode)
 
 	return dsn
 }
